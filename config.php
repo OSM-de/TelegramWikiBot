45b3e6063c801ba404da8706638632e689bbd7c2
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2020-06-22 07:25:15 +0200

    Create config.php
    Creation of cleared config.php so that the repo works after cloning, no real information in it though.

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Add your bot's API key and name
+$bot_api_key  = 'ENTER_YOUR:BOT_API_CODE_FROM_BOTFATHER';
+$bot_username = 'UserNameOfYourBot';
+
+// Define all IDs of admin users in this array (leave as empty array if not used)
+$admin_users = [
+	1234567890
+];
+
+?>
