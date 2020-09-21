0eede25190c1e24f4efd8f485d842ae23b2de092
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2020-09-21 21:18:05 +0200

    Update getUpdates.php
    Added CNWR handling, some refactoring

--- getUpdates.php
+++ getUpdates.php
@@ -9,6 +9,9 @@ require_once __DIR__ . '/config.php';
 // Preferred Language
 $pref_lang    = 'de';
 
+$sent_messages = json_decode(file_get_contents("sent_messages.json"));
+$old_message = "";
+
 try {
 	// Initialize Bot core
 	$telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
@@ -26,48 +29,76 @@ try {
 
 		// If a message was edited, we'll just send out another message with the new content
 		$this_message = (isset($this_update->edited_message) ? $this_update->edited_message : $this_update->message);
-		
+
 		// Chat and Message ID for later use
 		$chat_id = $this_message->chat->id;
 		$message_id = $this_message->message_id;
 		
-		// ... typing while I resolve all those links
-		Request::sendChatAction([
-			'chat_id' => $chat_id,
-			'action'  => Longman\TelegramBot\ChatAction::TYPING,
-		]);
-		
-		// Go through code/pre text entities 
-		if (isset($this_message->entities)) {
-			foreach($this_message->entities as $ent) {
-				if ($ent->type == "code" || $ent->type == "pre") {
-					$this_resolved = resolveOSMWikiLinks(substr($this_message->text, $ent->offset, $ent->length));
-					if (strpos($send_text, $this_resolved) === false) {
-						$send_text .= ($send_text != "" ? "\n" : "") . $this_resolved;
-					}
+		if (isset($this_update->edited_message)) {
+			foreach($sent_messages->sent_messages as $m) {
+				if ($m->chat_id == $chat_id && $m->reply_to_message_id == $message_id) {
+				$old_message = $m->text;
 				}
 			}
 		}
 		
+		// ... typing while I resolve all those links
+		/* 2020-07-09 removing Chat Action because it's annoying people.
+		Request::sendChatAction([
+			'chat_id' => $chat_id,
+			'action'  => Longman\TelegramBot\ChatAction::TYPING,
+		]);
+		*/
+
+		// Go through code/pre text entities 
+		$ent_array_in_msg = Array();
+		if (isset($this_message->entities)) {
+			foreach($this_message->entities as $ent) {
+				if ($ent->type == "code") {
+					$this_offset = intval($ent->offset);
+					if (strpos(substr($this_message->text, $this_offset, $ent->length), " ") !== false) {
+						$this_offset += strpos(substr($this_message->text, $this_offset, $ent->length), " ") + 1;
+					}
+					$ent_array_in_msg[] = Array($this_offset, intval($ent->length));
+				}
+			}
+			foreach($ent_array_in_msg as $ent) {
+				$this_resolved = resolveOSMWikiLinks(trim(substr($this_message->text, $ent[0], $ent[1])));
+				if (strpos($send_text, $this_resolved) === false) {
+					$send_text .= ($send_text != "" ? "\n" : "") . $this_resolved;
+				}
+			}
+		}
+
 		// Get other wiki links
 		$send_text .= ($send_text != "" ? "\n" : "") . resolveLinks($this_message->text);
-		// Clean up line breaks
+		
 		$send_text = preg_replace("/[\r?\n]+/", "\n", $send_text);
-
+		
 		// sending the result back to the chat
-		$send_result = Request::sendMessage([
-			'chat_id'				=> $chat_id,
-			'reply_to_message_id'	=> $message_id,
-			'parse_mode'			=> 'MarkdownV2',
-			'text'					=> $send_text
-		]);
+		if ($send_text != $old_message) {
+			try {
+				$send_result = Request::sendMessage([
+					'chat_id'				=> $chat_id,
+					'reply_to_message_id'	=> $message_id,
+					'parse_mode'			=> 'MarkdownV2',
+					'text'					=> $send_text
+				]);
+				$this_result = Array('chat_id' => $chat_id, 'reply_to_message_id' => $message_id, 'text' => $send_text);
+				$sent_messages->sent_messages[] = $this_result;
+				file_put_contents("sent_messages.json", json_encode($sent_messages));
+			} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+				syslog(LOG_INFO, $e->getMessage());
+			}
+		}
+
 	} else {
 		echo $server_response->printError();
 	}
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-	echo $e->getMessage();
+	syslog(LOG_INFO, $e->getMessage());
 } catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
-	echo $e->getMessage();
+	syslog(LOG_INFO, $e->getMessage());
 }
 
 function resolveLinks($text) {
@@ -78,6 +109,7 @@ function resolveLinks($text) {
 	// W:Q1234 : wikidata.org
 	// [[Tag]] : wiki.openstreetmap.org without pre-processing
 	// [[W:A]] : <preferredLanguage>.wikipedia.org
+	// (C|N|W|R)(#|/)1234 : Changeset, Node, Way, Relation #1234
 	preg_match_all("/((W:)?Q(\d+)(\#P(\d+))?|\[\[(W:)?([^\]]+)\]\])/", $text, $matches);
 	for($match_num = 0; $match_num < sizeof($matches[0]); $match_num++) {
 		if ($matches[3][$match_num] != "" && $matches[2][$match_num] == "") {
@@ -118,6 +150,13 @@ function resolveLinks($text) {
 				($returntext != "" ? "\n" : "") .
 				buildMarkdownLine($matches[0][$match_num], $page, $this_link, $src);
 		}
+	}
+	preg_match_all("/([CNWR])\#(\d+)/i", $text, $matches);
+	$link_abbr = Array("C" => "Changeset", "N" => "Node", "W" => "Way", "R" => "Relation");
+	for($match_num = 0; $match_num < sizeof($matches[0]); $match_num++) {
+		$title = getNWRInfo(strtolower($link_abbr[strtoupper($matches[1][$match_num])]), $matches[2][$match_num]);
+		$returntext .=
+			($returntext != "" ? "\n" : "") . $title;
 	}
 	return $returntext;
 }
@@ -169,20 +208,22 @@ function resolveOSMWikiLinks($text) {
 }
 
 function buildMarkdownLine($title, $page, $link, $src = null) {
-	if (trim($title) != "") return 
-		str_replace(
-			Array("=", "*", "[", "]", "#", "_"), 
-			Array("\\=", "\\*", "\\[", "\\]", "\\#", "\\_"), 
-			$title
-		) . 
-		($src != null ? " \(" . $src . "\)" : "").
-		": [" . 
-		str_replace(
-			Array("=", "*", "[", "]", "#", "_"), 
-			Array("\\=", "\\*", "\\[", "\\]", "\\#", "\\_"), 
-			$page
-		) . 
-		"]" . $link;
+	if (trim($title) != "") {
+		return 
+			replaceMarkdownCharacters($title) . 
+			($src != null ? " \(" . $src . "\)" : "").
+			": [" . 
+			replaceMarkdownCharacters($page) .
+			"]" . $link;
+	}
+}
+
+function replaceMarkdownCharacters($instr) {
+	return str_replace(
+		Array("=", "*", "[", "]", "#", "_", "|", ".", "(", ")"),
+		Array("\\=", "\\*", "\\[", "\\]", "\\#", "\\_", "\\|", "\\.", "\\(", "\\)"),
+		$instr
+	);
 }
 
 function wikiApiGetPage($wiki, $query) {
@@ -205,5 +246,30 @@ function wikiApiGetPage($wiki, $query) {
 		}
 	} else $return_page = "";
 	return $return_page;
+}
+
+function getNWRInfo($type, $id) {
+	$url = "https://api.openstreetmap.org/api/0.6/".$type."/".$id.".json";
+	$fc = file_get_contents($url);
+	$j = json_decode($fc);
+	$this_element = $j->elements[0];
+	if ($type != "changeset") {
+		$c_time = strtotime($this_element->timestamp);
+		$dt = new DateTime("@$c_time");
+		$dtp = strval($dt->format("d\\\.m\\\.Y \\u\\m H:i:s"));
+		$title = "[" . strtoupper(substr($type, 0, 1)) . substr($type, 1) . " " .
+			(isset($this_element->tags->name) ? "\"" . replaceMarkdownCharacters($this_element->tags->name) . "\"" : $this_element->id) . 
+			"](https://osm.org/$type/$id), geändert am " . $dtp . " " .
+			"zu Version " . $this_element->version . " mit [Changeset " . $this_element->changeset . "](https://osm.org/changeset/{$this_element->changeset}) von " .
+			"Benutzer [".replaceMarkdownCharacters($this_element->user)."](https://osm.org/user/{$this_element->user}})";
+	} else {
+		$c_time = strtotime($this_element->created_at);
+		$dt = new DateTime("@$c_time");
+		$dtp = strval($dt->format("d\\\.m\\\.Y \\u\\m H:i:s"));
+		$title = "[Changeset " . $this_element->id . "](https://osm.org/changeset/$id) " . 
+			"erstellt von Benutzer [".replaceMarkdownCharacters($this_element->user)."](https://osm.org/user/{$this_element->user}}) ".
+			"am " . $dtp;
+	}
+	return $title;
 }
 ?>
